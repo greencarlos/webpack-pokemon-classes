0bf10346ab740f70c1f7c47316d122adaf89b9d1
green.carlos <verdeclos@gmail.com>
2024-06-01 14:45:07 -0700

    updated stars

--- src/stars.js
+++ src/stars.js
@@ -3,9 +3,21 @@ import ReactDOM from "react-dom";
 
 const Stars = () => {
   const [value, setValue] = useState(0);
+  const [click, setClick] = useState(false);
+
+  function Header({ click, value }) {
+    if (click) {
+      return <h1>You have given {value} stars!</h1>;
+    }
+    return <h1>You are giving {value} stars</h1>;
+  }
 
   const stars = [1, 2, 3, 4, 5].map((star, i) => {
     let iconClass = star <= value ? "fas" : "far";
+
+    if (click) {
+      return <i className={`${iconClass} fa-star star`} key={star}></i>;
+    }
 
     return (
       <i
@@ -19,8 +31,15 @@ const Stars = () => {
   });
 
   return (
-    <div>
-      <h1>You are giving {value} stars</h1>
+    <div
+      onClick={() => {
+        setClick(true);
+      }}
+      onMouseLeave={() => {
+        setClick(false);
+      }}
+    >
+      <Header value={value} click={click} />
       <br />
       {stars}
     </div>
